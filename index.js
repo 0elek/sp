d922db270e789080c4671f41db4d5d8ce9caebe8
0elek <87550550+0elek@users.noreply.github.com>
2023-08-16 19:32:59 +0200

    y

--- index.js
+++ index.js
@@ -16,13 +16,32 @@ async function fetchWebApi(endpoint, method, body) {
 
 async function getTopTracks(){
   return (await fetchWebApi(
-    'v1/me/top/tracks?time_range=short_term&limit=50', 'GET'
+    'v1/me/top/tracks?time_range=short_term&limit=5', 'GET'
   )).items;
 }
+
 
 const topTracks = await getTopTracks();
 console.log(
   topTracks?.map(
+    ({name, artists}) =>
+      `${name} by ${artists.map(artist => artist.name).join(', ')}`
+  )
+);
+
+let topTracksIds = topTracks.map(({id}) => id);
+console.log(topTracksIds);
+
+async function getRecommendations(){
+    return (await fetchWebApi(
+      `v1/recommendations?limit=5&seed_tracks=${topTracksIds.join(',')}`, 'GET'
+    )).tracks;
+  }
+  
+
+const recommendedTracks = await getRecommendations();
+console.log(
+  recommendedTracks.map(
     ({name, artists}) =>
       `${name} by ${artists.map(artist => artist.name).join(', ')}`
   )
