ec07dc703c66a16573eb0e89312a2dd4982ca2dd
0elek <87550550+0elek@users.noreply.github.com>
2023-08-16 18:41:07 +0200

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+let token = process.env.SPOTIFY_TOKEN;  
+
+async function fetchWebApi(endpoint, method, body) {
+  const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    method,
+    body:JSON.stringify(body)
+  });
+  return await res.json();
+}
+
+async function getTopTracks(){
+  return (await fetchWebApi(
+    'v1/me/top/tracks?time_range=short_term&limit=50', 'GET'
+  )).items;
+}
+
+const topTracks = await getTopTracks();
+console.log(
+  topTracks?.map(
+    ({name, artists}) =>
+      `${name} by ${artists.map(artist => artist.name).join(', ')}`
+  )
+);
